f8e4178b7c7fac60011316d33c419b8a4ce1e93c
sefirus <sashasam348@gmail.com>
2022-08-14 13:27:14 +0300

    completed course

---
+++ M220N/Repositories/RepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace M220N.Repositories
+{
+    public static class RepositoryExtensions
+    {
+        public static void RegisterMongoDbRepositories(this IServiceCollection servicesBuilder)
+        {
+            servicesBuilder.AddSingleton<IMongoClient, MongoClient>(s =>
+            {
+                var uri = s.GetRequiredService<IConfiguration>()["MongoUri"];
+                var settings = MongoClientSettings.FromConnectionString(uri);
+                settings.MaxConnectionPoolSize = 50;
+
+                return new MongoClient(settings);
+            });
+            servicesBuilder.AddSingleton<MoviesRepository>();
+            servicesBuilder.AddSingleton<UsersRepository>();
+            servicesBuilder.AddSingleton<CommentsRepository>();
+            servicesBuilder.AddSingleton(s => s.GetRequiredService<IConfiguration>()["JWT_SECRET"]);
+        }
+    }
+}
